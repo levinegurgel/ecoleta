62bb24056379e01854042a62ca01694a6357d780
Levine Gurgel <levinegurgelf@gmail.com>
2021-02-09 16:59:01 -0400

    Adicionado componente header utilizando propriedades e uma pincelada de estado e imutabilidade

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import { count } from 'console';
+import React, {useState} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+
 
 function App() {
+
+  //[valor do estado, função para atualizar o valor do estado]
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick(){
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-      </header>
+    <div>
+      <Header title={`Contador ${counter}`}/> 
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }
